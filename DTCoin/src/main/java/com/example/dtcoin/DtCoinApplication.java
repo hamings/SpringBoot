3e146653e3da81f922c140dbc6e6017ccb0b2e88
hamings <wjdgpal9630@gmail.com>
2022-12-29 15:02:32 +0900

    feat: add mybatis

--- DTCoin/src/main/java/com/example/dtcoin/DtCoinApplication.java
+++ DTCoin/src/main/java/com/example/dtcoin/DtCoinApplication.java
@@ -1,8 +1,9 @@
 package com.example.dtcoin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan(value ={"com.example.dtcoin.mapper"})
 @SpringBootApplication
 public class DtCoinApplication {
 
