d52f10cbb20ec3197b69166ead8c8df9ba99f096
hamings <wjdgpal9630@gmail.com>
2023-01-05 15:43:17 +0900

    feat: add pagecontroller

---
+++ DTAdmin/src/main/java/com/example/dtadmin/controller/PageController.java
@@ -0,0 +1,44 @@
+package com.example.dtadmin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String gologin(){
+        return "login";
+    }
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/goMember")
+    public String goMember(){
+        return "memberList";
+    }
+
+    @GetMapping("/goPayment")
+    public String goPayment(){
+        return "paymentList";
+    }
+
+    @GetMapping("/goBuy")
+    public String goBuy(){
+        return "buyList";
+    }
+
+    @GetMapping("/goContact")
+    public String goContact(){
+        return "contactList";
+    }
+
+    @GetMapping("/goSubscribe")
+    public String goSubscribe(){
+        return "subscribeList";
+    }
+
+
+}
