2408934476e2d31fe1ff13030f6bc0d29604ae2d
hamings <wjdgpal9630@gmail.com>
2023-01-05 15:04:36 +0900

    Initial Commit

---
+++ DTAdmin/src/main/java/com/example/dtadmin/DtAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.dtadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtAdminApplication.class, args);
+    }
+
+}
---
+++ DTAdmin/src/test/java/com/example/dtadmin/DtAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dtadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
