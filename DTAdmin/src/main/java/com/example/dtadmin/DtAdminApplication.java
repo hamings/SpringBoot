781d67ded391579561ad5e2e01c1b9680a9bd8d9
hamings <wjdgpal9630@gmail.com>
2023-01-05 15:33:03 +0900

    feat: add mybatis

--- DTAdmin/src/main/java/com/example/dtadmin/DtAdminApplication.java
+++ DTAdmin/src/main/java/com/example/dtadmin/DtAdminApplication.java
@@ -1,9 +1,11 @@
 package com.example.dtadmin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = "com.example.dtadmin.mapper")
 public class DtAdminApplication {
 
     public static void main(String[] args) {
