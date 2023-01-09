4d8a043a1b1b0c5811e6e2a556f419affb2212bc
hamings <wjdgpal9630@gmail.com>
2023-01-09 14:14:20 +0900

    Initial Commit

---
+++ jpaTodolist/src/test/java/com/example/jpatodolist/JpaTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpatodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
