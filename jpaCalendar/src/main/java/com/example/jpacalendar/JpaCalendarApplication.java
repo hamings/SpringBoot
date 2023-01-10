3a609762f8ceb9a7097fa5dcdfb92f7092497a6d
hamings <wjdgpal9630@gmail.com>
2023-01-10 13:54:07 +0900

    Initial Commit

---
+++ jpaCalendar/src/main/java/com/example/jpacalendar/JpaCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpacalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaCalendarApplication.class, args);
+    }
+
+}
---
+++ jpaCalendar/src/test/java/com/example/jpacalendar/JpaCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpacalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
